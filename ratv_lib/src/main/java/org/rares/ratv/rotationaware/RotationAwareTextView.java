ae8d83fed0aefa8a7b863b6d07f96a027f66bf8e
rares-vmw <37401286+0x00FF00FF@users.noreply.github.com>
2018-11-01 15:11:46 +0200

    + fixed a problem with drawing the rotated view when the parent has a margin and the view has its width set using layout params. the text was not centered, instead it was off by the parent margin.

--- ratv_lib/src/main/java/org/rares/ratv/rotationaware/RotationAwareTextView.java
+++ ratv_lib/src/main/java/org/rares/ratv/rotationaware/RotationAwareTextView.java
@@ -152,10 +152,10 @@ public class RotationAwareTextView extends View {
     private final PointF cc = new PointF();
     private final PointF lc = new PointF();
 
-/*    private int leftMargin = 0;
+    private int leftMargin = 0;
     private int rightMargin = 0;
     private int topMargin = 0;
-    private int bottomMargin = 0;*/
+    private int bottomMargin = 0;
 
     public RotationAwareTextView(Context context) {
         super(context);
@@ -282,7 +282,7 @@ public class RotationAwareTextView extends View {
             sizeH = (int) Math.min((int) textPaint.getTextSize() * 1.25, sizeH);
         }
 
-        /*refreshParentMargins();*/
+        refreshParentMargins();
 
         setMeasuredDimension(sizeW, sizeH);
     }
@@ -312,8 +312,8 @@ public class RotationAwareTextView extends View {
      */
     @Override
     protected void onDraw(Canvas canvas) {
-        cc.x = (canvas.getWidth() /*- (leftMargin + rightMargin)*/) / 2;
-        cc.y = (canvas.getHeight()/* - (topMargin + bottomMargin)*/) / 2;
+        cc.x = (canvas.getWidth() - (leftMargin + rightMargin)) / 2;
+        cc.y = (canvas.getHeight() - (topMargin + bottomMargin)) / 2;
         lc.x = mLayout.getWidth() / 2;
         lc.y = mLayout.getHeight() / 2;
 
@@ -1035,14 +1035,14 @@ public class RotationAwareTextView extends View {
         return Layout.Alignment.ALIGN_CENTER;
     }
 
-    /*
+    /**
      * Added this because there is a problem with
      * laying out the view when its parent has
      * some horizontal margin and the view has fixed
      * width. when drawing, we need to take into
      * consideration the parent's width as well
      */
-/*    private void refreshParentMargins() {
+    private void refreshParentMargins() {
         ViewGroup.LayoutParams lp = ((View) getParent()).getLayoutParams();
         if (lp instanceof ViewGroup.MarginLayoutParams) {
             ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) lp;
@@ -1058,7 +1058,7 @@ public class RotationAwareTextView extends View {
                 bottomMargin = mlp.bottomMargin;
             }
         }
-    }*/
+    }
 
     /**
      * This kind of renders the view unusable.
